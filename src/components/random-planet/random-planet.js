925866892e6a5c5ed30b29e0960cb9ccfdccb79c
vladislav <verner.vladislav23@gmail.com>
2019-06-10 14:15:45 +0700

    Добавил отображение планет (random-planet.js), добавил функции в swapiservice

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -1,27 +1,52 @@
 import React, { Component } from 'react';
-
+import SwapiService from '../../services/swapi-service'
 import './random-planet.css';
 
 export default class RandomPlanet extends Component {
 
+  swapiService = new SwapiService()
+  
+  state = {
+    planet: {}
+  }
+
+  onPlanetLoaded = (planet) => {
+    this.setState({planet})
+  }
+
+  updatePlanet() {
+    const id = Math.floor(Math.random()*25 + 2)
+    this.swapiService.getPlanet(id)
+    .then((this.onPlanetLoaded))
+  }
+
+  constructor() {
+    super()
+    this.updatePlanet()
+  }
+
+
   render() {
+
+    const { planet: {id, name, population, rotationPeriod, diameter} } = this.state
+    console.log(`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`)
     return (
       <div className="random-planet jumbotron rounded">
-        <img className="planet-image"
-             src="https://starwars-visualguide.com/assets/img/planets/5.jpg" />
+        <img className="planet-image"  
+             src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
         <div>
-          <h4>Planet Name</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
-              <span className="term">Population</span>
-              <span>123124</span>
+              <span className="term">Population: {population}</span>
+              <span></span>
             </li>
             <li className="list-group-item">
-              <span className="term">Rotation Period</span>
+              <span className="term">Rotation Period: {rotationPeriod}</span>
               <span>43</span>
             </li>
             <li className="list-group-item">
-              <span className="term">Diameter</span>
+              <span className="term">Diameter: {diameter}</span>
               <span>100</span>
             </li>
           </ul>
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,6 +1,48 @@
-class SwapiService {
+export default class SwapiService {
     
     _apiBase = 'https://swapi.co/api'
+
+    _extractId(item) {
+        const idRegExp = /\/([0-9]*)\/$/
+        return item.url.match(idRegExp)[1]
+    }
+
+    _transformApiPlanet(planet) {
+        
+        return {
+            id: this._extractId(planet),
+            name: planet.name,
+            population: planet.population,
+            rotationPeriod: planet.rotationPeriod,
+            diameter: planet.diameter
+        }
+    }
+
+    _transformApiStarship(starship) {
+
+        return {
+            id: this._extractId(starship),
+            name: starship.name,
+            model: starship.model,
+            manufacturer: starship.manufacturer,
+            costInCredits: starship.costInCredits,
+            length: starship.length,
+            crew: starship.crew,
+            passengers: starship.passengers,
+            cargoCapacity: starship.cargoCapacity
+        }
+    }
+
+    _transformAPerson(person) {
+
+        return {
+            id: this._extractId(person),
+            name: person.name,
+            gender: person.gender,
+            birthYear: person.birthYear,
+            eyeColor: person.eyeColor
+        }
+    }
 
     async getResource(url)  {
         const res = await fetch(`${this._apiBase}${url}`)
@@ -14,29 +56,32 @@ class SwapiService {
 
     async getAllPeople() {
         const res = await this.getResource('/people/')
-        return res.results
+        return res.results.map(this._transformAPerson)
     }
 
     async getPerson(id) {
-        return this.getResource(`/people/${id}/`)
+        const person = this.getResource(`/people/${id}/`)
+        return this._transformAPerson(person)
     }
 
     async getAllPlanets() {
         const res = await this.getResource('/planets/')
-        return res.results
+        return res.results.map(this._transformApiPlanet)
     }
 
     async getPlanet(id) {
-        return this.getResource(`/planets/${id}`)
+        const planet = await this.getResource(`/planets/${id}`)
+        return this._transformApiPlanet(planet)
     }
 
     async getAllStarships() {
         const res = await this.getResource('/starships/')
-        return res.results
+        return res.results.ma(this._transformApiPlanet)
     }
 
     async getStarship(id) {
-        return this.getResource(`/starships/${id}`)
+        const starship = this.getResource(`/starships/${id}`)
+        return this._transformApiStarship(starship)
     }
 
 }
